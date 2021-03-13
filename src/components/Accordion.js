b02e318cf5f6a340a382e273c0d103c59baf9fcb
Milo Silva <milo.silva234@gmail.com>
2021-03-13 17:30:07 +0100

    Update Accordion component to support two sizes - create an optional size prop that supports 'small' & 'large' strings - use size prop to modify the fontSize of the Accordion title - handle invalid size values by throwing an errow.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { CardItem, Col, Icon, Text } from "native-base";
+import { StyleSheet } from "react-native";
 
 export const styleChevron = {
   color: "lightgray",
 };
+const sizeEnum = ["small", "large"];
 
 function ChevronIcon({ direction }) {
   return <Icon type="Ionicons" name={`chevron-${direction}-outline`} style={styleChevron} />;
 }
 
-function ChevronNav({ icon, text, direction, onPress }) {
+function ChevronNav({ style, icon, text, direction, onPress }) {
   return (
     <CardItem bordered button onPress={onPress}>
       {icon}
 
       <Col>
-        <Text style={{ fontSize: 26 }}>{text}</Text>
+        <Text style={style}>{text}</Text>
       </Col>
 
       <ChevronIcon direction={direction} />
@@ -23,14 +25,16 @@ function ChevronNav({ icon, text, direction, onPress }) {
   );
 }
 
-export default function Accordion({ icon, title, children }) {
+export default function Accordion({ icon, title, children, size = "large" }) {
   const [expanded, setExpanded] = useState(false);
-
+  const style = styles[size];
+  if (!style) throw Error("invalid Accordion size prop");
   return (
     <>
       <ChevronNav
         icon={icon}
         text={title}
+        style={style}
         direction={expanded ? "up" : "down"}
         onPress={() => setExpanded(!expanded)}
       />
@@ -39,3 +43,10 @@ export default function Accordion({ icon, title, children }) {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  small: {
+    fontSize: 18,
+  },
+  large: { fontSize: 26 },
+});
